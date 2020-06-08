8500a4d67c06b8573b1f02692d1095e2c8c817d3
Rohit <rohitpratti@gmail.com>
2020-06-07 22:40:22 -0700

    updated login route with validated form

--- brohitmedia-client/src/App.js
+++ brohitmedia-client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 // Pages
 import Home from "./Pages/home";
@@ -10,20 +12,35 @@ import Login from "./Pages/login";
 // Components
 import NavBar from "./Components/NavBar";
 
+const Theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#607d8b",
+    },
+    secondary: {
+      main: "#607d8b",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
 const App = () => {
   return (
-    <div>
-      <Router>
-        <NavBar />
-        <div className={"container"}>
-          <Switch>
-            <Route exact path={"/"} component={Home} />
-            <Route exact path={"/login"} component={Login} />
-            <Route exact path={"/signup"} component={Signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={Theme}>
+      <div className={"app"}>
+        <Router>
+          <NavBar />
+          <div className={"container"}>
+            <Switch>
+              <Route exact path={"/"} component={Home} />
+              <Route exact path={"/login"} component={Login} />
+              <Route exact path={"/signup"} component={Signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 };
 
--- brohitmedia-client/src/Pages/login.js
+++ brohitmedia-client/src/Pages/login.js
@@ -1,13 +1,167 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Avatar1 from "../images/femaleAvatar.png";
+import Avatar2 from "../images/maleAvatar.jpeg";
+import axios from "axios";
+// MUI stuff
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const avatarArr = [Avatar1, Avatar2];
+
+const styles = {
+  form: {
+    textAlign: "center",
+  },
+  image: {
+    height: "100px",
+    width: "100px",
+    borderRadius: "50%",
+    margin: "20px auto 20px auto",
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto",
+  },
+  textField: {
+    margin: "10px auto 10px auto",
+  },
+  button: {
+    marginTop: 20,
+    position: "relative",
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10,
+  },
+  progress: {
+    position: "absolute",
+  },
+};
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    loading: false,
+    errors: {},
+  };
+  random = (mn, mx) => {
+    return Math.random() * (mx - mn) + mn;
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post("/login", userData)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          loading: false,
+        });
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+        console.log(err.response);
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
   render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
     return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img
+            className={classes.image}
+            alt={"login"}
+            src={avatarArr[Math.floor(this.random(1, 3)) - 1]}
+          />
+          <Typography variant={"h2"} className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id={"email"}
+              name={"email"}
+              type={"email"}
+              label={"Email"}
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange}
+              helperText={errors.email}
+              error={!!errors.email}
+              fullWidth
+            />{" "}
+            <TextField
+              id={"password"}
+              name={"password"}
+              type={"password"}
+              label={"Password"}
+              className={classes.textField}
+              value={this.state.password}
+              onChange={this.handleChange}
+              helperText={errors.password}
+              error={!!errors.password}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography
+                variant={"body2"}
+                className={classes.customError}
+                value={errors.general}
+              >
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type={"submit"}
+              variant={"contained"}
+              color={"primary"}
+              className={classes.button}
+              disabled={loading}
+            >
+              Login
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <br />
+            <small>
+              Don't have an account? <Link to={"/signup"}>Sign Up!</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
